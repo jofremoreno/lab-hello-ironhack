44c6d8526411d31eab6996ce4542426996100cf4
Jofre <jofremoreno@gmail.com>
2019-10-03 14:39:22 +0200

    wip - voy haciendo. no he tenido tiempo y me salieron errores con npm start que hicieron que me retrasara...

---
+++ my-first-react-app/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+//lo que importo desde de la carpeta components a App.js
+import Header from './components/Header';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </Header>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-first-react-app/src/components/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Nav from './Nav';
+import Title from './Title'
+
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="App-header">
+      </div>
+    );
+  }
+}
+
+export default Header;
