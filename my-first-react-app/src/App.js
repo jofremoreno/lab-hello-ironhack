6421d14522f1da9bec56cbdd7e2e55ea2f9fa2fb
Jofre <jofremoreno@gmail.com>
2019-10-03 15:44:15 +0200

    wip - update navbar

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-//import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 //lo que importo desde de la carpeta components a App.js
-import Header from './Header';
-import Navbar from './Navbar';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
